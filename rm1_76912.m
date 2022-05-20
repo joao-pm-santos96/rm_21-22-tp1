c0fbae508480765f9092c47b3c8efea3d66f94c7
joao-pm-santos96 <joao.pm.santos96@gmail.com>
2022-05-20 10:09:32 +0100

    Start fixing IK

--- rm1_76912.m
+++ rm1_76912.m
@@ -200,86 +200,7 @@ function rm1_76912(N, Dt, r, L, Vn, Wn)
         disp('Step 5')
     end
 
-    % Differential drive
-    diff_wheels = [];
-    for n=1:1:size(control_inputs,1)
-
-        alpha = smooth_path(n,3);
-        transform = [cos(alpha) 0
-                sin(alpha) 0
-                0 1];
-
-        vels = transform * control_inputs(n,:)';
-        [wr, wl] = DiffDriveIK(r,L,vels(1),vels(2),vels(3));
-        diff_wheels = [diff_wheels; [wr, wl]];
-    end
-    writematrix(diff_wheels, DD_FILE);
     
-    if (DEBUG)
-        figure
-        plot(smooth_path(:,1), diff_wheels(:,1))
-        hold on
-        plot(smooth_path(:,1), diff_wheels(:,2))
-        legend('Right wheel', 'Left wheel')
-        title('Differential Drive')
-        grid on
-    end
-
-    % Tricycle
-    tri_wheels = [];
-    for n=1:1:size(control_inputs,1)
-        disp('Do me!!')
-    end
-    writematrix(tri_wheels, TRI_FILE)
-
-    % Omnidirectional drive
-    omni_wheels = [];
-    for n=1:1:size(control_inputs,1)
-
-        alpha = smooth_path(n,3);
-%         transform = [cos(alpha) -sin(alpha) 0
-%             sin(alpha) cos(alpha) 0
-%             0 0 1];
-        transform = [cos(alpha) 0
-                sin(alpha) 0
-                0 1]; % TODO is it this or the above?
-        
-        vels = transform * control_inputs(n,:)';
-        [w1, w2, w3] = OmniDriveIK(r,L,vels(1),vels(2),vels(3));
-        omni_wheels = [omni_wheels; [w1, w2, w3]];
-    end
-    writematrix(omni_wheels, OMNI_FILE);
-
-    if (DEBUG)
-        figure
-        plot(smooth_path(:,1), omni_wheels(:,1))
-        hold on
-        plot(smooth_path(:,1), omni_wheels(:,2))
-        hold on
-        plot(smooth_path(:,1), omni_wheels(:,3))
-        legend('Wheel 1', 'Wheel 2', 'Wheel 3')
-        title('Omnidrive')
-        grid on
-    end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -295,3 +216,75 @@ function rm1_76912(N, Dt, r, L, Vn, Wn)
 
 
 end
+
+% % Differential drive
+% diff_wheels = [];
+% for n=1:1:size(control_inputs,1)
+% 
+%     alpha = smooth_path(n,3);
+%     transform = [cos(alpha) 0
+%             sin(alpha) 0
+%             0 1];
+% 
+%     vels = transform * control_inputs(n,:)';
+%     [wr, wl] = DiffDriveIK(r,L,vels(1),vels(2),vels(3));
+%     diff_wheels = [diff_wheels; [wr, wl]];
+% end
+% writematrix(diff_wheels, DD_FILE);
+% 
+% if (DEBUG)
+%     figure
+%     plot(smooth_path(:,1), diff_wheels(:,1))
+%     hold on
+%     plot(smooth_path(:,1), diff_wheels(:,2))
+%     legend('Right wheel', 'Left wheel')
+%     title('Differential Drive')
+%     grid on
+% end
+% 
+% % Tricycle
+% tri_wheels = [];
+% for n=1:1:size(control_inputs,1)
+%     disp('Do me!!')
+% end
+% writematrix(tri_wheels, TRI_FILE)
+% 
+% %     if (DEBUG)
+% %         figure
+% %         plot(smooth_path(:,1), tri_wheels(:,1))
+% %         hold on
+% %         plot(smooth_path(:,1), tri_wheels(:,2))
+% %         legend('Wheel speed', 'Steering angle')
+% %         title('Tricycle')
+% %         grid on
+% %     end
+% 
+% % Omnidirectional drive
+% omni_wheels = [];
+% for n=1:1:size(control_inputs,1)
+% 
+%     alpha = smooth_path(n,3);
+% %         transform = [cos(alpha) -sin(alpha) 0
+% %             sin(alpha) cos(alpha) 0
+% %             0 0 1];
+%     transform = [cos(alpha) 0
+%             sin(alpha) 0
+%             0 1]; % TODO is it this or the above?
+%     
+%     vels = transform * control_inputs(n,:)';
+%     [w1, w2, w3] = OmniDriveIK(r,L,vels(1),vels(2),vels(3));
+%     omni_wheels = [omni_wheels; [w1, w2, w3]];
+% end
+% writematrix(omni_wheels, OMNI_FILE);
+% 
+% if (DEBUG)
+%     figure
+%     plot(smooth_path(:,1), omni_wheels(:,1))
+%     hold on
+%     plot(smooth_path(:,1), omni_wheels(:,2))
+%     hold on
+%     plot(smooth_path(:,1), omni_wheels(:,3))
+%     legend('Wheel 1', 'Wheel 2', 'Wheel 3')
+%     title('Omnidrive')
+%     grid on
+% end
