104daac80bad57bd31ee544069a27adbc42243f5
joao-pm-santos96 <joao.pm.santos96@gmail.com>
2022-05-29 20:39:30 +0100

    moved to single file

--- rm1_76912.m
+++ rm1_76912.m
@@ -32,10 +32,7 @@ function rm1_76912(N, Dt, r, L, Vn, Wn, vel)
     beacons = BeaconDetection(N);
 
     % Get all known points
-    beacon_poses = [];
-    for n=1:1:N
-        beacon_poses = [beacon_poses; beacons(n).X, beacons(n).Y, beacons(n).a];
-    end
+    beacon_poses = [beacons(:).X; beacons(:).Y; beacons(:).a]';
     known_poses = [INITIAL_POSE; beacon_poses];
 
     % Get intermediate points per section
@@ -59,7 +56,7 @@ function rm1_76912(N, Dt, r, L, Vn, Wn, vel)
     smooth_path = [all_poses(:,1), yq];
 
     % Get smooth path orientations
-    orients = [INITIAL_POSE(3)]; 
+    orients = INITIAL_POSE(3); 
     for n=2:1:size(smooth_path,1)
         p0 = smooth_path(n-1,:);
         p1 = smooth_path(n,:);
@@ -188,6 +185,7 @@ function rm1_76912(N, Dt, r, L, Vn, Wn, vel)
         plot(smooth_path(:,1), diff_wheels(:,1)) 
         hold on
         plot(smooth_path(:,1), diff_wheels(:,2))
+        grid on
         legend('wr', 'wl')
     end
     
@@ -204,7 +202,9 @@ function rm1_76912(N, Dt, r, L, Vn, Wn, vel)
         figure
         plot(smooth_path(:,1), tri_wheels(:,1)) 
         hold on
+        yyaxis right
         plot(smooth_path(:,1), tri_wheels(:,2))
+        grid on
         legend('wt', 'alpha')
     end
 
@@ -224,6 +224,7 @@ function rm1_76912(N, Dt, r, L, Vn, Wn, vel)
         plot(smooth_path(:,1), omni_wheels(:,2))
         hold on
         plot(smooth_path(:,1), omni_wheels(:,3))
+        grid on
         legend('w1', 'w2', 'w3')
     end
 
@@ -231,4 +232,192 @@ end
 
 %% ADDITIONAL FUNCTIONS
 
-% TODO add all funtions to this file!!
+function [x_t1, P_t1] = EKF(x_t, u_t, z_t1, P_t, lm_xy, Q, R, dt)
+% x_t: current pose estimate [x y theta]'
+% u_t: current control input [v w]'
+% z_k1: observations after moving to next pose N x [dist ang]
+% P_t: current variance/covariance
+% lm_xy: landmark positions [x y]
+% Q: covariance matrix for the process noise
+% R:covariance matrix for the observation noise
+    
+    %% Defaults
+    w_t = [0 0];
+    v_t = [0 0];
+
+    %% Prediction    
+    jfx = Jfx(x_t, u_t, w_t, dt);
+    jfw = Jfw(x_t, dt);
+
+    x_t1_pred = MotionModel(x_t, u_t, w_t, dt);
+    P_t1_pred = jfx * P_t * jfx' + jfw * Q * jfw';  
+
+    %% Update
+    jh = [];
+    for n=1:1:size(lm_xy,1)
+        jh = [jh; Jh(x_t1_pred, lm_xy(n,:))];
+    end
+
+    z_all = reshape(z_t1', [], 1);
+    z_pred = [];
+    for n=1:1:size(lm_xy,1)
+        z_pred = [z_pred; SensorModel(x_t1_pred, lm_xy(n,:), v_t)'];
+    end
+    innov = z_all - z_pred;
+
+    S = jh * P_t1_pred * jh' + R;
+    K = P_t1_pred * jh' * pinv(S);
+
+    x_t1 = (x_t1_pred' + K * innov)';
+    P_t1 = P_t1_pred - K * S * K';
+
+end
+
+function state_t1 = MotionModel(state_t, input_t, sigma, dt)
+% MotionModel Motion model for the general mobile robot in 2D
+%   state_t1 = MotionModel(prev_state, c_in, sigma, dt)
+%   
+%   prev_state: previous state [x, y, alpha]
+%   c_in: control input [v_lin, v_ang]
+%   sigma: linear and angular velocity uncertainty [sigma_lin, sigma_ang]
+%   dt: time interval
+    
+    T = [cos(state_t(3)) 0
+        sin(state_t(3)) 0
+        0 1];
+
+    state_t1 = ((T * (input_t + sigma)') .* dt + state_t')';
+
+end
+
+function z = SensorModel(state_t, lm_xy, sigma)
+% SensorModel Sensor model for LiDAR
+%   z = SensorModel(state_t, lm_xy, sigma)
+%
+%   state_t: robot position at time t [x, y]
+%   lm_xy: landmark position [x, y]
+%   sigma: distance and angle measure uncertainty [sigma_dist, sigma_angl]
+    
+    delta = lm_xy - state_t(1:2);
+
+    r = norm(delta);
+    phi = atan2(delta(2), delta(1)) - state_t(3);
+
+    z = [r phi] + sigma;
+
+end
+
+function jacob = Jfx(x_t, u_t, w_t, t)
+
+    jacob = [1 0 -t.*(w_t(1) + u_t(1)).*sin(x_t(3))
+        0 1 t.*(w_t(1) + u_t(1)).*cos(x_t(3))
+        0 0 1];
+end
+
+function jacob = Jfw(x_t, t)
+    
+    jacob = [t.*cos(x_t(3)) 0
+        t.*sin(x_t(3)) 0
+        0 t];
+end
+
+function jacob = Jh(x_t1_pred, lm_xy)
+
+    delta = x_t1_pred(1:2) - lm_xy;
+
+    a = (delta(1))./sqrt((-delta(1)).^2 + (-delta(2)).^2);
+    b = (delta(2))./sqrt((-delta(1)).^2 + (-delta(2)).^2);
+    c = 0;
+    d = (-delta(2))./((1 + (-delta(2)).^2./(-delta(1)).^2).*(-delta(1)).^2);
+    e = -1./((1 + (-delta(2)).^2./(-delta(1)).^2).*(-delta(1)));
+    f = -1;
+    
+    jacob = [a b c
+        d e f];
+
+end
+
+function inv_k = DiffDriveIK(R, L, start_pos, end_pos, dt)
+% DiffDriveIK Inverse Kinematics for the Differential Drive robot
+%   inv_k = DiffDriveIK(R, L, start_pos, end_pos, dt)
+%   RESTRICTION: wl ~= wr
+%
+%   R: wheel radius
+%   L: wheel base
+%   start_pos: starting pose [x0, y0, theta0]
+%   end_pos: ending pose [x1, y1, theta1]
+%   dt: delta time
+
+    X0 = start_pos(1);
+    % Y0 = start_pos(2);
+    TH0 = start_pos(3);
+
+    X = end_pos(1);
+    % Y = end_pos(2);
+    TH = end_pos(3);
+
+    if ((TH - TH0) ~= 0)
+        inv_k.wl = (-L.*TH.*sin(TH - TH0)/2 + L.*TH0.*sin(TH - TH0)/2 + TH.*X - TH.*X0 - TH0.*X + TH0.*X0)./(R.*dt.*sin(TH - TH0));
+        inv_k.wr = (L.*(TH - TH0).*sin(TH - TH0)/2 + TH.*X - TH.*X0 - TH0.*X + TH0.*X0)./(R.*dt.*sin(TH - TH0));
+    else
+        inv_k.wl = (-L.*TH + L.*TH0 + 2*X - 2*X0)./(2*R.*dt);
+        inv_k.wr = (L.*TH - L.*TH0 + 2*X - 2*X0)./(2*R.*dt);
+    end
+
+end
+
+function inv_k = OmniDriveIK(R, L, start_pos, end_pos, dt)
+% OmniDriveIK Inverse Kinematics for the Omnidirectional robot
+%   inv_k = OmniDriveIK(R, L, start_pos, end_pos, dt)
+%   RESTRICTION: w1 ~= -w2 + w3
+%
+%   R: wheel radius
+%   L: wheel base
+%   start_pos: starting pose [x0, y0, theta0]
+%   end_pos: ending pose [x1, y1, theta1]
+%   dt: delta time
+
+    X0 = start_pos(1);
+    Y0 = start_pos(2);
+    TH0 = start_pos(3);
+
+    X = end_pos(1);
+    Y = end_pos(2);
+    TH = end_pos(3);
+
+    inv_k.w1 = L.*TH./(R.*dt) - L.*TH0./(R.*dt) - Y./(R.*dt) + Y0./(R.*dt);
+    inv_k.w2 = L.*TH./(R.*dt) - L.*TH0./(R.*dt) + sqrt(3)*X./(2*R.*dt) - sqrt(3)*X0./(2*R.*dt) + Y./(2*R.*dt) - Y0./(2*R.*dt);
+    inv_k.w3 = -L.*TH./(R.*dt) + L.*TH0./(R.*dt) + sqrt(3)*X./(2*R.*dt) - sqrt(3)*X0./(2*R.*dt) - Y./(2*R.*dt) + Y0./(2*R.*dt);
+    
+end
+
+function inv_k = TricycleIK(R, L, start_pos, end_pos, dt)
+% TricycleIK Inverse Kinematics for the Tricyle robot
+%   inv_k = TricycleIK(R, L, start_pos, end_pos, dt)
+%   RESTRICTION: wt ~= 0 & alpha ~= 0
+%
+%   R: wheel radius
+%   L: wheel base
+%   start_pos: starting pose [x0, y0, theta0]
+%   end_pos: ending pose [x1, y1, theta1]
+%   dt: delta time
+
+    X0 = start_pos(1);
+    % Y0 = start_pos(2);
+    TH0 = start_pos(3);
+
+    X = end_pos(1);
+    % Y = end_pos(2);
+    TH = end_pos(3);
+
+    if ((X - X0 ~= 0) && (TH - TH0 ~= 0))
+        inv_k.alpha = atan(L.*sin(TH - TH0)./(X - X0));
+        inv_k.wt = (TH - TH0).*(X - X0)./(R.*dt.*sin(TH - TH0));
+    else
+        inv_k.alpha = atan(L.*(TH - TH0)./(X - X0));
+        inv_k.wt = (X - X0)./(R.*dt);
+    end
+
+end
+
+
